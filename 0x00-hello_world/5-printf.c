01f0893ddd9e4b9337daec235b1dae0b5268113b
Deanvetran <daniel.m.mutua001@gmail.com>
2023-09-08 06:33:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - a program that prints with printf function
+ *return: 0(success)
+ */
+int main(void)
+{
+Printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
